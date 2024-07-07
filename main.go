4d21c1856dea257da7374901aeb281dea17729ce
applyinnovations <alexander@applyinnovations.com.au>
2024-07-08 09:02:20 +1000

    pre-testing commit

--- main.go
+++ main.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 	"github.com/go-playground/webhooks/v6/github"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 	"os/exec"
 	"path/filepath"
+	"time"
 )
 
 const (
 	WEBHOOK_PATH = "/webhooks"
-	CACHE_PATH   = "cache"
+	CACHE_DIR    = "cache"
+	LOG_DIR      = "log"
 )
 
 type Context struct {
@@ -21,7 +25,7 @@ type Context struct {
 	repoSha       string
 	branchSha     string
 	commitSha     string
-	deploymentSha string
+	repoBranchSha string
 }
 
 func generateContext(repo, branch, commitSha string) Context {
@@ -29,14 +33,14 @@ func generateContext(repo, branch, commitSha string) Context {
 	// generate inputs to handlers
 	repoSha := sha256.Sum256([]byte(repo))
 	branchSha := sha256.Sum256([]byte(branch))
-	deploymentSha := sha256.Sum256([]byte(fmt.Sprintf("%s%s", repo, branch)))
+	repoBranchSha := sha256.Sum256([]byte(fmt.Sprintf("%s%s", repo, branch)))
 	return Context{
 		repo,
 		branch,
 		commitSha,
 		string(repoSha[:]),
 		string(branchSha[:]),
-		string(deploymentSha[:]),
+		string(repoBranchSha[:]),
 	}
 }
 
@@ -44,27 +48,35 @@ func generateContext(repo, branch, commitSha string) Context {
 func handleUp(ctx Context) error {
 
 	// clone/pull to cache/repos/repo/docker-compose.yml
-	cacheDir := filepath.Join(CACHE_PATH, ctx.repoSha, ctx.branchSha, ctx.commitSha)
-	cloneCmd := exec.Command("git", "clone", "--branch", ctx.branch, "--single-branch", ctx.repo, cacheDir)
-	cloneCmd.Stdout = log.Writer()
-	cloneCmd.Stderr = log.Writer()
-	if err := cloneCmd.Run(); err != nil {
+	cacheDir := filepath.Join(CACHE_DIR, ctx.repoSha, ctx.branchSha, ctx.commitSha)
+
+	defer func() {
+		err := os.RemoveAll(cacheDir)
+		if err != nil {
+			log.Printf("failed to remove `%s`\n", cacheDir)
+		}
+	}()
+
+	cmd := exec.Command("git", "clone", "--branch", ctx.branch, "--single-branch", ctx.repo, cacheDir)
+	cmd.Stdout = log.Writer()
+	cmd.Stderr = log.Writer()
+	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed `git clone`: %w", err)
 	}
 
 	// validate docker compose file
-	validateCmd := exec.Command("docker", "compose", "--project-directory", cacheDir, "config")
-	validateCmd.Stdout = log.Writer()
-	validateCmd.Stderr = log.Writer()
-	if err := validateCmd.Run(); err != nil {
+	cmd = exec.Command("docker", "compose", "--project-directory", cacheDir, "config")
+	cmd.Stdout = log.Writer()
+	cmd.Stderr = log.Writer()
+	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed `docker compose config`: %w", err)
 	}
 
 	// docker compose up -p sha256(org/repo/branch)
-	upCmd := exec.Command("docker", "compose", "--project-directory", cacheDir, "--project-name", ctx.deploymentSha)
-	upCmd.Stdout = log.Writer()
-	upCmd.Stderr = log.Writer()
-	if err := upCmd.Run(); err != nil {
+	cmd = exec.Command("docker", "compose", "--project-directory", cacheDir, "--project-name", ctx.repoBranchSha)
+	cmd.Stdout = log.Writer()
+	cmd.Stderr = log.Writer()
+	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed `docker compose up`: %w", err)
 	}
 
@@ -75,7 +87,7 @@ func handleUp(ctx Context) error {
 func handleDown(ctx Context) error {
 
 	// stop containers
-	cmd := exec.Command("docker", "container", "stop", fmt.Sprintf("$(docker ps -q -f name=%s)", ctx.deploymentSha))
+	cmd := exec.Command("docker", "container", "stop", fmt.Sprintf("$(docker ps -q -f name=%s)", ctx.repoBranchSha))
 	cmd.Stdout = log.Writer()
 	cmd.Stderr = log.Writer()
 	if err := cmd.Run(); err != nil {
@@ -83,7 +95,7 @@ func handleDown(ctx Context) error {
 	}
 
 	// rm containers
-	cmd = exec.Command("docker", "container", "rm", fmt.Sprintf("$(docker ps -a -q -f name=%s)", ctx.deploymentSha))
+	cmd = exec.Command("docker", "container", "rm", fmt.Sprintf("$(docker ps -a -q -f name=%s)", ctx.repoBranchSha))
 	cmd.Stdout = log.Writer()
 	cmd.Stderr = log.Writer()
 	if err := cmd.Run(); err != nil {
@@ -91,7 +103,7 @@ func handleDown(ctx Context) error {
 	}
 
 	// rm network
-	cmd = exec.Command("docker", "network", "rm", fmt.Sprintf("$(docker network ls -q -f name=%s)", ctx.deploymentSha))
+	cmd = exec.Command("docker", "network", "rm", fmt.Sprintf("$(docker network ls -q -f name=%s)", ctx.repoBranchSha))
 	cmd.Stdout = log.Writer()
 	cmd.Stderr = log.Writer()
 	if err := cmd.Run(); err != nil {
@@ -99,7 +111,7 @@ func handleDown(ctx Context) error {
 	}
 
 	// rm volume
-	cmd = exec.Command("docker", "volume", "rm", fmt.Sprintf("$(docker volume ls -q -f name=%s)", ctx.deploymentSha))
+	cmd = exec.Command("docker", "volume", "rm", fmt.Sprintf("$(docker volume ls -q -f name=%s)", ctx.repoBranchSha))
 	cmd.Stdout = log.Writer()
 	cmd.Stderr = log.Writer()
 	if err := cmd.Run(); err != nil {
@@ -110,16 +122,51 @@ func handleDown(ctx Context) error {
 }
 
 func main() {
-	l := log.New(os.Stderr)
+	err := os.MkdirAll(LOG_DIR, 0755)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create `%s` directory: %v", LOG_DIR, err))
+	}
+
+	logFilename := fmt.Sprintf("%s.log", time.Now().Format("20060102-150405"))
+	logFile, err := os.OpenFile(filepath.Join(LOG_DIR, logFilename), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(fmt.Sprintf("failed to open log file `%s`: %v", logFilename, err))
+	}
+	defer logFile.Close()
+
+	jsonHandler := slog.NewJSONHandler(os.Stderr, nil)
+	logger := slog.New(jsonHandler)
+	slog.SetDefault(logger)
+
+	err = os.MkdirAll(CACHE_DIR, 0755)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create `%s` directory: %v", CACHE_DIR, err))
+	}
+
 	hook, _ := github.New(github.Options.Secret("?"))
 	http.HandleFunc(WEBHOOK_PATH, func(w http.ResponseWriter, r *http.Request) {
-		payload, err := hook.Parse(r, github.PushEvent)
+		payload, err := hook.Parse(r, github.PushEvent, github.DeleteEvent)
 		if err != nil {
 			if err == github.ErrEventNotFound {
 				// event out of scope
 			}
 		}
 		switch payload.(type) {
+
+		case github.CreatePayload:
+
+			// deploy latest
+			create := payload.(github.CreatePayload)
+			if create.RefType == "branch" {
+				repo := create.Repository.CloneURL
+				branch := create.Ref
+				ctx := generateContext(repo, branch, "")
+				err := handleUp(ctx)
+				if err != nil {
+					log.Println("failed `handleUp`: %w", err)
+				}
+			}
+
 		case github.PushPayload:
 
 			// deploy latest
@@ -133,10 +180,19 @@ func main() {
 				log.Println("failed `handleUp`: %w", err)
 			}
 
-		case github.ReleasePayload:
-		//	release := payload.(github.ReleasePayload)
-		case github.PullRequestPayload:
-			//	pullRequest := payload.(github.PullRequestPayload)
+		case github.DeletePayload:
+
+			// clean up releases
+			del := payload.(github.DeletePayload)
+			if del.RefType == "branch" {
+				repo := del.Repository.CloneURL
+				branch := del.Ref
+				ctx := generateContext(repo, branch, "")
+				err := handleDown(ctx)
+				if err != nil {
+					log.Println("failed `handleDown`: %w", err)
+				}
+			}
 		}
 	})
 	http.ListenAndServe(":3000", nil)
