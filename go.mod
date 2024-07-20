97183cc342ff4092b41feb9d948ba0f565ae1219
applyinnovations <alexander@applyinnovations.com.au>
2024-07-20 18:14:32 +1000

    attempted to implement secret handler

--- go.mod
+++ go.mod
@@ -1,5 +1,12 @@
 module github.com/applyinnovations/cicd
 
-go 1.21.10
+go 1.22.1
 
-require github.com/go-playground/webhooks/v6 v6.3.0
+toolchain go1.22.5
+
+require (
+	github.com/go-playground/webhooks/v6 v6.3.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require github.com/stretchr/testify v1.9.0 // indirect
--- util.go
+++ util.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
 	"log"
 	"os/exec"
+	"strings"
 )
 
-func execCmd(command string, args ...string) error {
+func execCmd(out io.Writer, command string, args ...string) error {
 	cmd := exec.Command(command, args...)
-	cmd.Stdout = log.Writer()
+	cmd.Stdout = out
 	cmd.Stderr = log.Writer()
 	return cmd.Run()
+}
+
+func generateContext(repo, ref, commitSha string) Context {
+	// generate inputs to handlers
+	branch := strings.TrimPrefix(ref, "refs/heads/")
+	repoSha := sha256.Sum256([]byte(repo))
+	branchSha := sha256.Sum256([]byte(branch))
+	repoBranchSha := sha256.Sum256([]byte(fmt.Sprintf("%s%s", repo, branch)))
+	return Context{
+		repo,
+		branch,
+		commitSha,
+		hex.EncodeToString(repoSha[:]),
+		hex.EncodeToString(branchSha[:]),
+		hex.EncodeToString(repoBranchSha[:]),
+	}
 }
